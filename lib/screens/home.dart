fb27f9fc560c9982d5b1f0f8f5a3ceea1929988d
nayemalam110 <nayemalam110@gmail.com>
2022-10-03 16:49:00 +0600

    step-2.1

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,60 +2,64 @@ import 'package:flutter/material.dart';
 import '../model/todo_list_model.dart';
 import 'package:provider/provider.dart';
 
-class Home extends StatelessWidget {
-  final _formKey = GlobalKey<FormState>();
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   TextEditingController _taskController = TextEditingController();
 
   void addList(String li, context) {
-    Provider.of<todoList>(context, listen: false).addTodo(li);
-    Navigator.of(context).pop();
+    setState(() {
+      Provider.of<todoList>(context, listen: false).addTodo(li);
+      Navigator.of(context).pop();
+      _taskController.clear();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final loadData = Provider.of<todoList>(context);
     return Scaffold(
-      body: ListView.builder(
-        itemCount: loadData.listitem.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Center(
-            child: Card(
-              child: Text(loadData.listitem[index].todoListtask),
-            ),
-          );
-        },
-      ),
+      body: ListViewWidget(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => showDialog(
             builder: (BuildContext context) {
               return AlertDialog(
                 title: Text('To Do'),
-                content: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _taskController,
-                        // validator: (value) {
-                        //   if (value!.isEmpty) {
-                        //     return 'Cannot be empty';
-                        //   } else {
-                        //     return null;
-                        //   }
-                        // },
-                      ),
-                      ElevatedButton(
-                        onPressed: () => addList(_taskController.text, context),
-                        child: const Text('save'),
-                      ),
-                    ],
-                  ),
+                content: Column(
+                  children: [
+                    TextFormField(
+                      controller: _taskController,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => addList(_taskController.text, context),
+                      child: const Text('save'),
+                    ),
+                  ],
                 ),
               );
             },
             context: context),
       ),
+    );
+  }
+}
+
+class ListViewWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var loadData = Provider.of<todoList>(context);
+    return ListView.builder(
+      itemCount: loadData.listitem.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Center(
+          child: Card(
+            child: Text(loadData.listitem[index].todoListtask),
+          ),
+        );
+      },
     );
   }
 }
